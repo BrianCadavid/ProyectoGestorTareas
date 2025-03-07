45efae3e256a66fd261fae5e9a0e37f3f62ebf0d
Cat0428 <lopezcardonacristiandavid326@gmail.com>
2025-03-07 18:47:04 -0500

    Cambio de color del boton del loggin

--- ProyectoGestor/GestorTareas/GestorTareas/Form1.Designer.cs
+++ ProyectoGestor/GestorTareas/GestorTareas/Form1.Designer.cs
@@ -86,7 +86,7 @@
             // 
             // btnIngresar
             // 
-            this.btnIngresar.BackColor = System.Drawing.Color.GreenYellow;
+            this.btnIngresar.BackColor = System.Drawing.Color.Khaki;
             this.btnIngresar.Font = new System.Drawing.Font("Lucida Bright", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnIngresar.Location = new System.Drawing.Point(125, 286);
             this.btnIngresar.Name = "btnIngresar";
