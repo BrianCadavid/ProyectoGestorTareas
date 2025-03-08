8e7231e4b3326e6493d9c632611f9933c2215508
Brian Cadavid <briancadavid@outlook.com>
2025-03-07 22:10:49 -0500

    Se cambio el boton loggin

--- ProyectoGestor/GestorTareas/GestorTareas/Form1.Designer.cs
+++ ProyectoGestor/GestorTareas/GestorTareas/Form1.Designer.cs
@@ -86,7 +86,7 @@
             // 
             // btnIngresar
             // 
-            this.btnIngresar.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnIngresar.BackColor = System.Drawing.Color.GreenYellow;
             this.btnIngresar.Font = new System.Drawing.Font("Lucida Bright", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnIngresar.Location = new System.Drawing.Point(133, 297);
             this.btnIngresar.Name = "btnIngresar";
